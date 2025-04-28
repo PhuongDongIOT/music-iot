e72ccacb1ad1e748e79522c03487e6f43825dab1
DongIOT <94212576+TRINHDINHDAI2306@users.noreply.github.com>
2025-04-28 22:21:37 +0700

    init source

---
+++ constants/index.ts
@@ -0,0 +1,38 @@
+
+export const sliderData = [
+    {
+        img: "/1.png",
+        location: "HIỀN",
+        description: "Em như dòng nước trong veo Xoá hết ưu phiền băng qua ngọn núi vòng vèo",
+        title: "Bình yên",
+        song: "/music/song-1.mp3"
+    },
+    {
+        img: "/2.png",
+        location: "HIỀN",
+        description: "Có ánh mắt câu thơ dịu dàng",
+        title: "Lhông yêu em thì yêu ai",
+        song: "/music/song-2.mp3"
+    },
+    {
+        img: "/3.png",
+        title: "Anh nhớ ra",
+        description: "Anh nhớ ra rằng mình biết yêu em là gì",
+        location: "HIỀN",
+        song: "/music/song-3.mp3"
+    },
+    {
+        img: "/4.png",
+        title: "Bước qua mùa cô đơn",
+        description: "Chào hàng cây, làm sao cố níu tay nhau lại để thấy nồng nàn, đang về trên đôi mắt em",
+        location: "HIỀN",
+        song: "/music/song-4.mp3"
+    },
+    {
+        img: "/7.png",
+        title: "Lạ lùng",
+        description: "Lạ lùng Em với gió hát lên câu ca làm Anh thao thức",
+        location: "HIỀN",
+        song: "/music/song-5.mp3"
+    },
+];
