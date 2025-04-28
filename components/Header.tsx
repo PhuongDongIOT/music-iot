8fc32963dd084114c35891fa9ff830f35980970c
DongIOT <94212576+TRINHDINHDAI2306@users.noreply.github.com>
2025-04-28 22:52:44 +0700

    fix: meta header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,9 +5,6 @@ import Head from "next/head";
 function Header() {
   return (
     <div className=" absolute mt-5 flex w-full flex-wrap items-center justify-between gap-2 px-5 text-xs font-medium uppercase opacity-90 md:px-10">
-      <Head>
-        <title>Cool Project</title>
-      </Head>
       <div className="  flex items-center gap-2 font-medium tracking-[4px]">
         <IoIosGlobe className=" text-xl" />
         Album For You
